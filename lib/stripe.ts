f0e4818cfc576ba5b0d1bb0a50becf0b917d5540
Yonatan <redonaroll09@gmail.com>
2026-05-26 13:50:02 +0400

    auto-save 2026-05-26T13:50:02

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -8,4 +8,6 @@ export const PRICE_ONE_TIME = process.env.STRIPE_PRICE_ID_ONE_TIME!
 export const PRICE_MONTHLY = process.env.STRIPE_PRICE_ID_MONTHLY!
 export const PRICE_ANNUAL = process.env.STRIPE_PRICE_ID_ANNUAL!
 
-export const ALLOWED_PRICES = new Set([PRICE_ONE_TIME, PRICE_MONTHLY, PRICE_ANNUAL])
+export const ALLOWED_PRICES = new Set(
+  [PRICE_ONE_TIME, PRICE_MONTHLY, PRICE_ANNUAL].filter(Boolean) as string[]
+)
